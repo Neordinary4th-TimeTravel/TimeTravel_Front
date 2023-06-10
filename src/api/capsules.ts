e3ab89def829a0813115b7bda4f0fe145e66e1ba
heehminh <mink141416@gmail.com>
2023-06-11 08:43:16 +0900

    [FEAT] DTO

--- src/api/capsules.ts
+++ src/api/capsules.ts
@@ -123,3 +123,122 @@ export async function postCapuslesLike({...data}: ToggleCapsuleLikeReqDto) {
   const response = await client.post(`/capsules/capsule`, data);
   return response.data;
 }
+
+interface PatchNicknameReqDto {
+  memberIdx: number;
+  targetNickname: string;
+}
+
+export async function petchModeNicknmae({...data}: PatchNicknameReqDto) {
+  const response = await client.patch(`/member/member/mod-nickname`, data);
+  return response.data;
+}
+
+interface CapDto {
+  creationDateTime: string;
+  releaseDateTime: string;
+  capsuleTitle: string;
+}
+
+interface CapResDto {
+  closedList: CapDto;
+  openedList: CapDto;
+}
+
+interface CapResReqDto {
+  memberIdx: number;
+}
+
+export async function getMemberCapsules({memberIdx}: CapResReqDto) {
+  const params: CapResReqDto = {memberIdx};
+  const response = await client.get<CapResDto>(`/member/capsules`, {
+    params,
+  });
+  return response.data;
+}
+
+export async function getMemberCapsulesTag({memberIdx}: CapResReqDto) {
+  const params: CapResReqDto = {memberIdx};
+  const response = await client.get<CapResDto>(`/member/capsules`, {
+    params,
+  });
+  return response.data;
+}
+
+export async function getMemberCapsulesSight({memberIdx}: CapResReqDto) {
+  const params: CapResReqDto = {memberIdx};
+  const response = await client.get<CapResDto>(`/member/capsules`, {
+    params,
+  });
+  return response.data;
+}
+
+interface FindScrapCategoryResDto {
+  categoryNameList: string[];
+}
+
+export async function getCapsulesScrap({memberIdx}: CapResReqDto) {
+  const params: CapResReqDto = {memberIdx};
+  const response = await client.get<FindScrapCategoryResDto>(
+    `/capsules/scrap`,
+    {
+      params,
+    },
+  );
+  return response.data;
+}
+
+export async function getCapsulesLike({memberIdx}: CapResReqDto) {
+  const params: CapResReqDto = {memberIdx};
+  const response = await client.get<CapResDto>(`/member/capsules/like`, {
+    params,
+  });
+  return response.data;
+}
+
+export async function getCapsulesCommnet({memberIdx}: CapResReqDto) {
+  const params: CapResReqDto = {memberIdx};
+  const response = await client.get<CapResDto>(`member/capsules/commnet`, {
+    params,
+  });
+  return response.data;
+}
+
+export async function getCapsulesClose({memberIdx}: CapResReqDto) {
+  const params: CapResReqDto = {memberIdx};
+  const response = await client.get<ViewImminentCapsuleResDto>(
+    `/member/capsules/close`,
+    {
+      params,
+    },
+  );
+  return response.data;
+}
+
+export async function getCapsulesCategorySearch({
+  categoryIdx,
+  scrollSize,
+}: FindPostByCategoryReqDto) {
+  const params: FindPostByCategoryReqDto = {categoryIdx, scrollSize};
+  const response = await client.get<FindPostByCategoryResDto>(
+    `/capsules/category/search`,
+    {
+      params,
+    },
+  );
+  return response.data;
+}
+
+export async function getCapsulesCategorySearchText({
+  postTitleList,
+  nextCursor,
+}: FindPostByTextResDto) {
+  const params: FindPostByTextResDto = {postTitleList, nextCursor};
+  const response = await client.get<FindPostByTextResDto>(
+    `/capsules/category/search/text`,
+    {
+      params,
+    },
+  );
+  return response.data;
+}
